27c15e61aa1155180721095560f796b5aa447cf3
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-06-28 15:38:19 -0500

    basic html, styling and fruits array

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function randomNumber(min, max) {
-	return Math.floor(Math.random() * (1 + max - min) + min);
-}
+$(function(){
+	var fruits = [];
+
+	fruits.push(new Fruit('Apples', 2));
+	fruits.push(new Fruit('Bananas', .75));
+	fruits.push(new Fruit('Grapes', 3.01));
+	fruits.push(new Fruit('Oranges', 1.58));
+
+	//test that our fruits array is working
+	//console.log(fruits[0].name);
+
+	function randomNumber(min, max) {
+		return Math.floor(Math.random() * (1 + max - min) + min);
+	}
+
+	function Fruit(name, price){
+		this.name = name;
+		this.price = price;
+	}
+});
